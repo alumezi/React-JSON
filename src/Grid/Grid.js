d90d4930ae1bc1b9ddf819b5d3f8b0c404d3769e
alumezi <arbnorlumezi@gmail.com>
2018-11-25 13:19:41 +0100

    changing layout

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,62 +1,58 @@
-import React, { Component } from 'react';
-import GridElement from './components/GridElements/GridElement';
-import GridDetail from './components/GridDetails/GridDetail';
+import React, { Component } from "react";
+import GridElement from "./components/GridElements/GridElement";
+import GridDetail from "./components/GridDetails/GridDetail";
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      displayOverlay : 'none',
-      displayId : ''
-    }
-
-    this.openOverlay = this.openOverlay.bind(this);
-    this.closeOverlay = this.closeOverlay.bind(this);
-    this.toggleOverlay = this.toggleOverlay.bind(this);
+      displayOverlay: "none",
+      displayId: ""
+    };
   }
 
-  openOverlay(displayId){
-    this.setState({displayId : displayId, displayOverlay : 'block'});
-  }
+  openOverlay = displayId => {
+    this.setState({ displayId: displayId, displayOverlay: "block" });
+  };
 
-  closeOverlay(){
-    this.setState({displayOverlay : 'none'});
-  }
-  
-  toggleOverlay(displayId){
-    this.setState({displayId});
-  }
+  closeOverlay = () => {
+    this.setState({ displayOverlay: "none" });
+  };
 
-
+  toggleOverlay = displayId => {
+    this.setState({ displayId });
+  };
 
   render() {
-
     const { GridElements, GridDetails } = this.props.section;
-    let { Children : ElementsChildren } = GridElements;
-    let { Children : DetailsChildren } = GridDetails;
+    let { Children: ElementsChildren } = GridElements;
+    let { Children: DetailsChildren } = GridDetails;
 
     let ElementsKeys = Object.keys(ElementsChildren);
     let DetailsKeys = Object.keys(DetailsChildren);
-    
 
-    
-      return (
-        <div className="App">
-             {ElementsKeys.map(key => <GridElement  data={ElementsChildren[key]} key={key} openOverlay={this.openOverlay} id={key} /> )}
-             {this.state.displayId ? 
-              <GridDetail  
-                data={DetailsChildren} 
-                keys={DetailsKeys} 
-                closeOverlay={this.closeOverlay} 
-                toggleOverlay={this.toggleOverlay}
-                display={this.state.displayOverlay} 
-                id={this.state.displayId}  />
-                :
-                null
-              }
-        </div>
-      );
+    return (
+      <div className="App">
+        {ElementsKeys.map(key => (
+          <GridElement
+            data={ElementsChildren[key]}
+            key={key}
+            openOverlay={this.openOverlay}
+            id={key}
+          />
+        ))}
+        {this.state.displayId ? (
+          <GridDetail
+            data={DetailsChildren}
+            keys={DetailsKeys}
+            closeOverlay={this.closeOverlay}
+            toggleOverlay={this.toggleOverlay}
+            display={this.state.displayOverlay}
+            id={this.state.displayId}
+          />
+        ) : null}
+      </div>
+    );
   }
 }
 
--- src/Grid/components/GridElements/GridElement.js
+++ src/Grid/components/GridElements/GridElement.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import './GridElement.css';
+import React from "react";
+import "./GridElement.css";
 
+const GridElement = props => {
+  const { id, data, openOverlay } = props;
+  const { type, value } = data.Content;
 
-const GridElement = (props) => {
-     
-        const { id, data, openOverlay } = props;
-        const { type, value } = data.Content;
-        
-        return (
-            <div className='container' id={id}>
-                <img src={value.background} alt=""  />
-                <div className="centered">
-                    <h1 className="title" >{value.title}</h1>
-                    { type === 'ImageBox' ? 
-                        <div className="read">
-                            <p>{value.teaser}</p>
-                            <p onClick={() => openOverlay(id)} className="read-more" >{value.link}</p>
-                        </div> 
-                        : 
-                        <a href={value.url} target="_blank" rel="noopener noreferrer" className="button">{value.link}</a> 
-                    }
-                </div>
-            </div>
-        )
-    }
-
-    
+  return (
+    <article className="container" id={id}>
+      <img src={value.background} alt="" />
+      <div className="centered">
+        <h1 className="title">{value.title}</h1>
+        {type === "ImageBox" ? (
+          <div className="read">
+            <p>{value.teaser}</p>
+            <p onClick={() => openOverlay(id)} className="read-more">
+              {value.link}
+            </p>
+          </div>
+        ) : (
+          <a
+            href={value.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button"
+          >
+            {value.link}
+          </a>
+        )}
+      </div>
+    </article>
+  );
+};
 
 export default GridElement;
